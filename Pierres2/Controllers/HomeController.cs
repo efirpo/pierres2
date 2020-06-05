62500735acba0537d388cc9a29975eef2db47bca
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-06-05 15:09:32 -0700

    edit styles, fix links

--- Pierres2/Controllers/HomeController.cs
+++ Pierres2/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Pierres2.Models;
+using System.Linq;
 
 namespace Pierres2.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly Pierres2Context _db;
+
+    public HomeController(Pierres2Context db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.Treats = _db.Treats.ToList();
       return View();
     }
   }
